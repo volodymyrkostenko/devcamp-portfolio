0366c7f3343374b9d3ff24dfab416755ba4f4c4d
volodymyrpodilskyy <volodymyrpodilskyy@gmail.com>
2018-01-17 12:27:09 +0200

    add destroy method to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,10 +22,17 @@ class PortfoliosController < ApplicationController
 	def edit
 
 	end
+	def destroy
+		@portfolio.destroy
+		 respond_to do |format|
+		    format.html { redirect_to portfolios_path, notice: 'Record was remove.' }
+		 end
+
+	end
 	def update
 		respond_to do |format|
 	      if @portfolio.update(portfolio_params)
-	        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+	        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
 	      else 
 	        format.html { render :edit }
 	      end
